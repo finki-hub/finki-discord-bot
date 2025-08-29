316e2c9ec909bb08d816485df294bbc1d67a855a
Delemangi <milev.stefan@gmail.com>
2025-08-29 17:18:53 +0200

    Enable ticket types to have description

--- src/lib/schemas/Ticket.ts
+++ src/lib/schemas/Ticket.ts
@@ -1,11 +1,10 @@
 import { z } from 'zod';
 
-export const TicketSchema = z
-  .object({
-    id: z.string(),
-    name: z.string(),
-    roles: z.array(z.string()),
-  })
-  .strict();
+export const TicketSchema = z.object({
+  description: z.string().optional(),
+  id: z.string(),
+  name: z.string(),
+  roles: z.array(z.string()),
+});
 
 export type Ticket = z.infer<typeof TicketSchema>;
