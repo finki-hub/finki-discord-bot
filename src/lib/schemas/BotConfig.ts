3179a62d7b7afe2ff05f29e60c0fa0ec1232af65
Delemangi <milev.stefan@gmail.com>
2025-08-29 16:55:23 +0200

    Remove strict object checking for entire bot configuration

--- src/lib/schemas/BotConfig.ts
+++ src/lib/schemas/BotConfig.ts
@@ -6,71 +6,62 @@ import { ModelSchema } from './Model.js';
 import { RoleSchema } from './Role.js';
 import { TicketSchema } from './Ticket.js';
 
-const TemporaryChannelConfigSchema = z
-  .object({
-    cron: z.string(),
-    name: z.string(),
-    parent: z.string().optional(),
-  })
-  .strict();
+const TemporaryChannelConfigSchema = z.object({
+  cron: z.string(),
+  name: z.string(),
+  parent: z.string().optional(),
+});
 
 const ModelsSchema = z.object({
   embeddings: ModelSchema.optional(),
   inference: ModelSchema.optional(),
 });
 
-export const RequiredBotConfigSchema = z
-  .object({
-    ads: z.array(AdSchema).optional(),
-    channels: z.record(ChannelSchema, z.string().optional()).optional(),
-    crossposting: z
-      .object({
-        channels: z.array(z.string()).optional(),
-        enabled: z.boolean().optional(),
-      })
-      .strict()
-      .optional(),
-    experience: z
-      .object({
-        enabled: z.boolean().optional(),
-        multipliers: z.record(z.string(), z.number()).optional(),
-      })
-      .strict()
-      .optional(),
-    guild: z.string().optional(),
-    intervals: z
-      .object({
-        buttonIdle: z.number().optional(),
-        ephemeralReply: z.number().optional(),
-        sendReminders: z.number().optional(),
-        ticketsCheck: z.number().optional(),
-      })
-      .strict()
-      .optional(),
-    models: ModelsSchema.optional(),
-    oathEnabled: z.boolean().optional(),
-    reactions: z
-      .object({
-        add: z.record(z.string(), z.string()).optional(),
-        remove: z.record(z.string(), z.string()).optional(),
-      })
-      .strict()
-      .optional(),
-    roles: z.record(RoleSchema, z.string()).optional(),
-    temporaryChannels: z
-      .record(TemporaryChannelSchema, TemporaryChannelConfigSchema)
-      .optional(),
-    themeColor: z.string().optional(),
-    ticketing: z
-      .object({
-        allowedInactivityDays: z.number().optional(),
-        enabled: z.boolean().optional(),
-        tickets: z.array(TicketSchema).optional(),
-      })
-      .strict()
-      .optional(),
-  })
-  .strict();
+export const RequiredBotConfigSchema = z.object({
+  ads: z.array(AdSchema).optional(),
+  channels: z.record(ChannelSchema, z.string().optional()).optional(),
+  crossposting: z
+    .object({
+      channels: z.array(z.string()).optional(),
+      enabled: z.boolean().optional(),
+    })
+    .optional(),
+  experience: z
+    .object({
+      enabled: z.boolean().optional(),
+      multipliers: z.record(z.string(), z.number()).optional(),
+    })
+    .optional(),
+  guild: z.string().optional(),
+  intervals: z
+    .object({
+      buttonIdle: z.number().optional(),
+      ephemeralReply: z.number().optional(),
+      sendReminders: z.number().optional(),
+      ticketsCheck: z.number().optional(),
+    })
+    .optional(),
+  models: ModelsSchema.optional(),
+  oathEnabled: z.boolean().optional(),
+  reactions: z
+    .object({
+      add: z.record(z.string(), z.string()).optional(),
+      remove: z.record(z.string(), z.string()).optional(),
+    })
+    .optional(),
+  roles: z.record(RoleSchema, z.string()).optional(),
+  temporaryChannels: z
+    .record(TemporaryChannelSchema, TemporaryChannelConfigSchema)
+    .optional(),
+  themeColor: z.string().optional(),
+  ticketing: z
+    .object({
+      allowedInactivityDays: z.number().optional(),
+      enabled: z.boolean().optional(),
+      tickets: z.array(TicketSchema).optional(),
+    })
+    .optional(),
+});
 
 export const BotConfigSchema = RequiredBotConfigSchema.optional();
 export type BotConfig = z.infer<typeof BotConfigSchema>;
