12f42bf560d16d407cfc047ed729933bb24326b4
Delemangi <milev.stefan@gmail.com>
2025-12-31 06:13:54 +0100

    Update about command

--- src/translations/about.ts
+++ src/translations/about.ts
@@ -3,9 +3,9 @@ import { hyperlink, userMention } from 'discord.js';
 export const botName = 'ФИНКИ Discord Бот';
 
 export const aboutMessage = (helpCommand: string, faqCommand: string) =>
-  `Овој бот е развиен од ${userMention(
+  `Овој Дискорд бот е развиен од ${userMention(
     '198249751001563136',
-  )} за потребите на Discord серверот на студентите на ФИНКИ. Ботот е open source и може да се најде на ${hyperlink(
+  )} за потребите на ${hyperlink('ФИНКИ Хаб', 'https://finki-hub.mk')}, независната студентска организација. Кодот е отворен и се наоѓа на ${hyperlink(
     'GitHub',
     'https://github.com/finki-hub/finki-discord-bot',
   )}. Ако имате било какви прашања, предлози или проблеми, контактирајте нè на Discord или на GitHub. \n\nНапишете ${helpCommand} за да ги видите сите достапни команди, или ${faqCommand} за да ги видите сите достапни прашања.`;
